c66c6bdb8fca613a54fcf54665fe420c481320d6
calderov <calderov@gmail.com>
2023-10-01 15:53:53 -0700

    Implement orientable camera

--- src/camera.h
+++ src/camera.h
@@ -10,29 +10,36 @@
 
 class camera {
   private:
-    point3d center;       // Camera center
-    point3d pixel00_loc;  // Location of pixel 0, 0
+    point3d center;         // Camera center
+    point3d pixel00_loc;    // Location of pixel 0, 0
     vector3d pixel_delta_u; // Offset to pixel to the right
     vector3d pixel_delta_v; // Offset to pixel below
+    vector3d u, v, w;       // Camera frame basis vectors
 
     void initialize() {
         // Setup viewport
-        double focal_length = 1.0;
+        center = lookfrom;
+        double focal_length = (lookfrom - lookat).length();
         double theta = degrees_to_radians(vfov);
         double h = tan(theta / 2);
         double viewport_height = 2.0 * h * focal_length;
         double viewport_width = viewport_height * (static_cast<double>(image_width) / image_height);
 
+        // Calculate the u, v, w unit basis vectors for the camera coordinate frame
+        w = unit_vector(lookfrom - lookat);
+        u = unit_vector(cross(vup, w));
+        v = cross(w, u);
+
         // Calculate the vectors across the horizontal and down the vertical viewport edges
-        vector3d viewport_u = vector3d(viewport_width, 0, 0);
-        vector3d viewport_v = vector3d(0, -viewport_height, 0);
+        vector3d viewport_u = viewport_width * u;
+        vector3d viewport_v = viewport_height * -v;
 
         // Calculate the horizontal and vertical delta vectors from pixel to pixel
         pixel_delta_u = viewport_u / image_width;
         pixel_delta_v = viewport_v / image_height;
 
         // Calculate the location of the upper left pixel
-        point3d viewport_upper_left = center - vector3d(0, 0, focal_length) - viewport_u / 2 - viewport_v / 2;
+        point3d viewport_upper_left = center - (focal_length * w) -  viewport_u / 2 -  viewport_v / 2;
         pixel00_loc = viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
     }
 
@@ -83,6 +90,10 @@ class camera {
     int samples_per_pixel = 10;  // Random samples per pixel
     int max_depth = 10;          // Maximum number of ray bounces into scene
     double vfov = 90;            // Vertical view angle (field of view)
+
+    point3d lookfrom = point3d(0, 0, -1); // Point camera is looking from
+    point3d lookat   = point3d(0, 0, 0);  // Point camera is looking at
+    vector3d vup     = vector3d(0, 1, 0); // Camera-relative "up" direction
 
     void render(const hittable& world) {
         initialize();
